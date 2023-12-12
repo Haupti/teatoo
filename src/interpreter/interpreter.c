e1956e728bba23dbe9353bbe7caf7c3de4585bc9
marwin <marwin.ack@googlemail.com>
2023-12-13 00:31:40 +0100

    think about how to interpret

--- src/interpreter/interpreter.c
+++ src/interpreter/interpreter.c
@@ -1,7 +1,22 @@
 #include "../parser/parser.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+void error_on_byte_expect_sequence(Argument arg){
+    if(arg.is_byte){
+        printf("ERROR: expected a sequence, but received a byte");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int iterpret_exec(Op_EXEC exec){
+    error_on_byte_expect_sequence(exec.first);
+    return EXIT_FAILURE;
+}
 
 int interpret(Module mod){
-    // run the module from here if it has exec, do nothing if it has not
+    if(mod.has_entrypoint){
+        interpret_exec(mod.entrypoint);
+    }
     return EXIT_SUCCESS;
 }
